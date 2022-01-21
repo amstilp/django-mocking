394d0f746a4f3c8b17649deaa98b7882a775a6ae
Adrienne Stilp <amstilp@uw.edu>
2022-01-20 16:32:09 -0800

    Add mocking to the pytest version of the test
    Add the pytest-mock package to the local requirements file, since
    it provides a mocker fixture for pytest. Update the test to mock
    the GET request.

--- django_mocking/cats/tests/test_views_pytest.py
+++ django_mocking/cats/tests/test_views_pytest.py
@@ -4,7 +4,11 @@ from django.urls import reverse
 
 pytestmark = pytest.mark.django_db
 
-def test_randomcat(client):
+def test_randomcat(client, mocker):
+    mock_response = mocker.patch('requests.get')
+    mock_response.status_code = 200
+    mock_response.json_response = '{}'
+    # Start the test.
     url = reverse('randomcat')
     response = client.get(url)
     assert response.status_code == 200
