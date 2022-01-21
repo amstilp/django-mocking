4a98c1e6048354d370a7d08fd3774d2197b289dd
Adrienne Stilp <amstilp@uw.edu>
2022-01-20 16:08:09 -0800

    Mock the GET request for the unittest version of the test

--- django_mocking/cats/tests/test_view_unittest.py
+++ django_mocking/cats/tests/test_view_unittest.py
@@ -1,3 +1,5 @@
+from unittest import mock
+
 from django.test import TestCase
 from django.urls import reverse
 
@@ -5,7 +7,12 @@ from django.urls import reverse
 class RandomCatTest(TestCase):
     """Unit tests for RandomCat view"""
 
-    def test_view(self):
+    @mock.patch('requests.get')
+    def test_view(self, mock_response):
+        # Set up mock request
+        mock_response.return_value.status_code = 200
+        mock_response.json.return_value = {'test': 'test2'}
+        # Start the test.
         response = self.client.get(reverse('randomcat'))
         self.assertEqual(response.status_code, 200)
         print('json response', response.context[1]['json_response'])
