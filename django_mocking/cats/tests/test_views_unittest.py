7ab1bf1a684f09981e5f60e50cb2704fc8d8eddc
Adrienne Stilp <amstilp@uw.edu>
2022-02-15 09:56:14 -0800

    Add a view test using the RequestFactory and mocking

--- django_mocking/cats/tests/test_views_unittest.py
+++ django_mocking/cats/tests/test_views_unittest.py
@@ -1,14 +1,15 @@
 from unittest import mock
 
-from django.test import TestCase
+from django.test import RequestFactory, TestCase
 from django.urls import reverse
 
+from django_mocking.cats.views import RandomCat
 
 class RandomCatTest(TestCase):
     """Unit tests for RandomCat view"""
 
     @mock.patch("requests.get")
-    def test_view(self, mock_response):
+    def test_view_with_client(self, mock_response):
         # Set up mock request
         mock_response.return_value.status_code = 200
         mock_response.json.return_value = {"test": "test2"}
@@ -16,3 +17,14 @@ class RandomCatTest(TestCase):
         response = self.client.get(reverse("randomcat"))
         self.assertEqual(response.status_code, 200)
         print("json response", response.context[1]["json_response"])
+
+    @mock.patch("requests.get")
+    def test_view_with_request_factory(self, mock_response):
+        # Set up mock request
+        mock_response.return_value.status_code = 200
+        mock_response.json.return_value = {"test": "test2"}
+        # Start the test.
+        request = RequestFactory().get(reverse("randomcat"))
+        response = RandomCat.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+        print("json response", response.context_data['json_response'])
